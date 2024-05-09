737b5f6f9231e0e227b30ffd91c5da81105243cd
Waheed10 <119417355+Waheed10@users.noreply.github.com>
2024-05-10 02:03:49 +0500

    s

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: use_key_in_widget_constructors, library_private_types_in_public_api, sized_box_for_whitespace, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
   @override
@@ -8,32 +9,33 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  final CameraPosition _initialCameraPosition = CameraPosition(
+    target: LatLng(37.7833, -122.4167), // Replace with your desired location
+    zoom: 11.0,
+  );
+
+  GoogleMapController? _mapController;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Live Google Map'),
+      ),
       body: Stack(
         children: <Widget>[
-          Container(
-            height: double.infinity,
-            width: double.infinity,
-            child: Image.asset(
-              'assets/images/mapbig.png',
-              fit: BoxFit.contain,
-            ),
+          GoogleMap(
+            initialCameraPosition: _initialCameraPosition,
+            onMapCreated: (GoogleMapController controller) {
+              _mapController = controller;
+            },
           ),
           Positioned(
             left: 140,
             bottom: 10,
-            child: GestureDetector(
-              onTap: () {
-                Navigator.pop(context);
-              },
-              child: Container(
-                child: Image.asset(
-                  'assets/images/closeiconmap.png',
-                  height: 100,
-                ),
-              ),
+            child: FloatingActionButton(
+              onPressed: () => Navigator.pop(context),
+              child: Icon(Icons.close),
             ),
           ),
         ],
